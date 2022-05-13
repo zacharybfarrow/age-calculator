6bde7582e9ce7e48d5c1bfd7cf65ecc69c46739a
zacharybfarrow <zachary.b.farrow@gmail.com>
2022-05-13 18:45:25 -0400

    basic age calculation implemented

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from datetime import date
+from dateutil.relativedelta import relativedelta
 
 # TODO: Age calculation
 birthday = date(1987, 11, 15)
 today = date.today()
 
-age_in_days = today - birthday
-print(age_in_days)
-
-# Not accurate - does not account for whether birthday has passed this year or not.
-age_in_years = today.year - birthday.year
-print(age_in_years)
-
-# Count years, looking for leap years along the way
+age = relativedelta(today, birthday)
+print(f"Current age is {age.years} years, {age.months} months, and {age.days} days.")
