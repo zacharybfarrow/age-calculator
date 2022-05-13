b07a0e6461c9a3f6325621a7084a022485807a1e
zacharybfarrow <zachary.b.farrow@gmail.com>
2022-05-13 19:08:00 -0400

    handle user input exceptions

--- app.py
+++ app.py
@@ -2,10 +2,29 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 
 
-# Get birthday from user
-month = int(input("Birth month: "))
-day = int(input("Birth day: "))
-year = int(input("Birth year: "))
+# Get birthday from user, handle exceptions
+
+while True:
+    try:
+        month = abs(int(input("Birth month: ")))
+        break
+    except ValueError:
+        print("Please enter numeric value.")
+
+while True:
+    try:
+        day = abs(int(input("Birth day: ")))
+        break
+    except ValueError:
+        print("Please enter numeric value.")
+
+while True:
+    try:
+        year = abs(int(input("Birth year (YYYY): ")))
+        break
+    except ValueError:
+        print("Please enter numeric value.")
+
 birthday = date(year, month, day)
 today = date.today()
 
