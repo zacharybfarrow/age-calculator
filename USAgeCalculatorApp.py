ce3ea3e71ab91437ffc528008235c4eee507b2e7
zacharybfarrow <zachary.b.farrow@gmail.com>
2022-05-31 13:02:57 -0400

    Implement basic age calculation functionality

--- USAgeCalculatorApp.py
+++ USAgeCalculatorApp.py
@@ -1,8 +1,29 @@
 from kivy.app import App 
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import NumericProperty, ObjectProperty
+
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
 
 class Interface(ScreenManager):
-    pass
+
+    month_input = ObjectProperty(None)
+    day_input = ObjectProperty(None)
+    year_input = ObjectProperty(None)
+
+    def calculate_age(self):
+        birth_month = int(self.month_input.text)
+        birth_day = int(self.day_input.text)
+        birth_year = int(self.year_input.text)
+        birthday = date(birth_year, birth_month, birth_day)
+        today = date.today()
+
+        age = relativedelta(today, birthday)
+
+        result = f"Current age is {age.years} years, {age.months} months, and {age.days} days."
+        print(result)
+        return result
 
 class MonthScreen(Screen):
     pass
@@ -19,6 +40,7 @@ class ResultScreen(Screen):
 class USAgeCalculatorApp(App):
     def build(self):
         return Interface()
+
 
 if __name__ == "__main__":
     USAgeCalculatorApp().run()
